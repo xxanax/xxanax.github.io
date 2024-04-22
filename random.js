89f9bec7941202969b50c89607e1d9e06d4672c1
xxanax <kolosoek86@gmail.com>
2024-04-22 18:22:20 -0400

    New page /random

---
+++ random.js
@@ -0,0 +1,20 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+let btn = document.getElementById("generate");
+
+btn.addEventListener('click', function(){ //вешаем событие на нажатие html-кнопки
+	let min = parseInt(document.getElementById("min").value);
+	let max = parseInt(document.getElementById("max").value);
+	let random = 0;
+	if (Number.isInteger(min) && Number.isInteger(max)){
+		random = Math.floor(Math.random() * (max - min + 1) + min);
+	} else {
+		random = 0; 
+	};
+
+	let result_div = document.getElementById("random_number")
+
+	result_div.innerText = random;
+
+});
